473e1b5317bf5635da5bac5d2788f8c404d958f1
Maryam Rezaee <ms.maryamrezaee@gmail.com>
2025-11-21 02:23:33 +0330

    feat: expose numpy engine components

---
+++ src/numpy_engine/__init__.py
@@ -0,0 +1,5 @@
+from .network import NumpyMLP
+from .layers import Linear
+from .activations import ReLU, Sigmoid, Tanh, LeakyReLU, SELU, Softmax
+from .loss import CrossEntropyLoss, MSELoss
+from .optimizers import SGD, Momentum, Adam
